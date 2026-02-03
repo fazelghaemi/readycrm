02444b99df1f983c4fde72640abb3d7eac209b05
fazelghaemi <ghaemipm@gmail.com>
2026-02-03 15:35:31 +0330

    Delete public directory

--- public/assets/codehub.js
+++
@@ -1,6 +0,0 @@
-function copyToClipboard(elId){
-  const el=document.getElementById(elId);
-  if(!el) return;
-  const text = el.innerText || el.textContent || '';
-  navigator.clipboard.writeText(text).then(()=>{ alert("کد در کلیپ‌بورد کپی شد"); });
-}
